072c746a5dc5f4cd4f3ecd8571994378b01389a8
eric zhang <ezhang323@gmail.com>
2019-02-16 01:20:26 -0800

    works now

--- chat.py
+++ chat.py
@@ -7,7 +7,7 @@ import chat_server
 def start_up(username):
     da_connection = chat_server.create_connection_obj()
 
-    print(da_connection.first_message(username))
+    da_connection.first_message(username)
 
     return da_connection
 
