ebba95d080e18e78bf1da20c7e4b43f867fda3f6
mengvf <mengvf@uci.edu>
2019-02-16 14:10:54 -0800

    fixed chatinput

--- chat_input.py
+++ chat_input.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-HELLO = None
-try:
-    from PIL import Image
-except:
-    HELLO = 'image failed.'
-=======
 import filetester
->>>>>>> 1f355195d1875b182511c7bec29f88f4131628d6
-
 def user_name():
     return input("Insert user name?:  ")
 def chat():
@@ -15,13 +6,7 @@ def chat():
 def emoji():
     return input("Insert an emoji: ")
 def picture():
-<<<<<<< HEAD
-    if HELLO != 'image failed':
-        img = Image.open(input("Insert an image: "))
-        img.show()
-=======
     filetester.checker(input("Insert an image: "))
->>>>>>> 1f355195d1875b182511c7bec29f88f4131628d6
 def file():
     return input("Insert a file: ")
 def nickname():
