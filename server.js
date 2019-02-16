e581aa7d561c6289761d2c786ba9ce7e72b7eade
Max <klinman.max@gmail.com>
2019-02-15 23:27:44 -0800

    echo server

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+http.createServer(function(request,response){
+  request.on("data", (msg) => {
+    response.write(msg);
+  });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  request.on("end", () => {
+    response.end();
+  })
+}).listen(port, hostname);
