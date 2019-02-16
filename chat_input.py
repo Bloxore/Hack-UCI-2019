8c077a00bc67741a8c7d022a3bb60f8984e29c1c
mengvf <mengvf@uci.edu>
2019-02-15 23:52:36 -0800

    I added emoji and picture functions.

--- chat_input.py
+++ chat_input.py
@@ -2,3 +2,7 @@ def user_name():
     return input("What is your user name?:  ")
 def chat():
     return input("What do you want to say?: ")
+def emoji():
+    return input("Insert an emoji: ")
+def picture():
+    return input("Insert an image: ")
