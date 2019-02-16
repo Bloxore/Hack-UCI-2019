cfa74ea9d5768845cfadc900b422bd1706a5b6d1
Max <klinman.max@gmail.com>
2019-02-16 12:46:57 -0800

    fixed it again again

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const server = net.createServer((socket) => {
       listeners.push(socket);
     }
     else if (data.toString().split(" ")[0] == "USER") {
-      userData = data.toString().split(" ")[1];
+      let userData = data.toString().split(" ")[1];
       username = "";
       for (let i = 1; i< userData.length; i++) {
         username += userData[i];
